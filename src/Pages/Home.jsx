15e5a29097c8dca20622a1e1f92f87ebe0bb71d4
walid hasan <walid.official8@gmail.com>
2024-11-03 14:48:37 +0600

    tab is customized

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Banner from '../components/Banner';
 import Category from '../components/Category';
 import { Outlet, useLoaderData } from 'react-router-dom';
+import Title from '../components/Title';
 
 const Home = () => {
     const categories = useLoaderData();
     return (
         <div>
             <Banner></Banner>
-            <Category categories={categories}></Category>
+            <div className="">
+                <div className="text-center">
+                    <Title title="This is a title"></Title>
+                </div>
+                <Category categories={categories}></Category>
+            </div>
             <Outlet></Outlet>
         </div>
     );
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 
 
+
 const Category = ({categories}) => {
   return (
-    <div className="w-11/12 mx-auto">
-      <div className="">
+    <div className="w-11/12 mx-auto my-12">
+      <div role="tablist" className="tabs tabs-lifted">
         {
-            categories.map((category,index) => <NavLink key={index} to={`CategoryCoffee/${category.category}`} role="tab" className={({isActive}) => `tab text-2xl font-thin ${isActive ? 'active' : ''}`}>
+            categories.map((category,index) => <NavLink key={index} to={`CategoryCoffee/${category.category}`} role="tab" className={({isActive}) => `tab text-2xl font-thin ${isActive ? 'tab-active' : ''}`}>
                 {category.category}
             </NavLink>)
         }
